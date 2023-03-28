c8dba578adde5f71d8e454306a2caca8679e81cc
samcms1234 <samdevpro99@gmail.com>
2023-03-29 00:43:07 +0530

    Added a Navbar component
        The React module is imported, along with several other modules such as useState, useEffect, Image, Link, and various icons from the react-icons library.
    
        A functional component called Navbar is defined using an arrow function that returns a JSX expression.
    
        Inside the Navbar component, several state variables are defined using the useState hook, such as discover, help, notification, profile, and openSideMenu. These variables will be used to keep track of the state of the component and determine what content should be displayed.
    
        Several functions are defined within the component to modify the state variables. For example, the openMenu function is called when the user clicks on the Discover or Help Center buttons and changes the state of the discover and help variables depending on which button was clicked.
    
        The openNotification function is called when the user clicks on the notification icon, and it toggles the state of the notification variable to either show or hide the notification content.
    
        The openProfile function is called when the user clicks on the user profile icon, and it toggles the state of the profile variable to either show or hide the profile content.
    
        The openSidebar function is called when the user clicks on the menu icon and toggles the state of the openSideMenu variable to either show or hide the sidebar content.
    
        The JSX expression returned by the Navbar component defines the structure and appearance of the navigation bar. It contains several nested div elements that are styled using CSS modules.
    
        The Image component is used to display the logo and user profile images, and the Link component is used to create clickable links within the navigation bar.
    
        Several components such as Discover, HelpCenter, Notification, Profile, and Sidebar are conditionally rendered based on the state variables defined earlier.
    
        Finally, the Navbar component is exported as the default component of this module, making it available for use in other parts of the application.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,158 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import image from 'next/image';
+import Link from 'next/link';
+import {MdNotifications} from 'react-icons/md';
+import {BsSearch} from 'react-icons/bs';
+import { CgMenuLeft, CgMenuRight } from 'react-icons/cg';
+import { Discover, HelpCenter, Notification, Profile, Sidebar } from './index'
+import Button from '../componentIndex';
+import images from '../images';
 
 import Style from './Navbar.module.css';
 
 const Navbar = () => {
+  const [discover, setDiscover] = useState(false);
+  const [help, setHelp] = useState(false);
+  const [notification, setNotification] = useState(false);
+  const [profile, setProfile] = useState(false);
+  const [openSideMenu, setOpenSideMenu] = useState(false);
+
+  const openMenu = (e) => {
+    const btnText = e.target.innerText;
+
+    if( btnText == "Discover" ) {
+      setDiscover(true);
+      setHelp(false);
+      setNotification(false);
+      setProfile(false);
+    }
+    else if(btnText == "Help Center") {
+      setDiscover(false);
+      setHelp(true);
+      setNotification(false);
+      setProfile(false);
+    }
+    else {
+      setDiscover(false);
+      setHelp(false);
+      setNotification(false);
+      setProfile(false);
+    }
+  };
+
+  const openNotification = () => {
+    if(!notification) {
+      setNotification(true);
+      setDiscover(false);
+      setHelp(false);
+      setProfile(false);
+    } else {
+      setNotification(false);
+    }
+  };
+
+  const openProfile = () => {
+    if(!profile) {
+      setProfile(true);
+      setHelp(false);
+      setDiscover(fales);
+      setNotification(fales);
+    } else {
+      setProfile(false);
+    }
+  }
+
+  const openSidebar = () => {
+    if(!openSideMenu) {
+      setOpenSideMenu(true);
+    } else {
+      setOpenSideMenu(false);
+    }
+  }
+
   return (
-    <div>Navbar</div>
+    <div className={Style.navbar}>
+      <div className={Style.navbar_container}>
+        <div className={Style.navbar_container_left}>
+          <div className={Style.logo}>
+            <Image src={images.logo}
+            alt="NFT Marketplace"
+            width={100}
+            height={100}
+            />
+            <div className={Style.navbar_container_left_box_input}>
+              <div className={Style.navbar_container_left_box_input_box}>
+                <input type='text' placeholder='Search NFT' />
+                <BsSearch onClick={() => {}} className={Style.search_com} />
+          </div>
+        </div>
+
+        <div className={Style.navbar_container_right}>
+          <div className={Style.nav_container_right_discover}>
+            <p onClick={(e) => openMenu(e)}>Discover</p>
+            {
+              discover && (
+                <div className={Style.nav_container_right_discover_box}>
+                  <Discover />
+                </div>
+            )}
+          </div>
+
+          <div className={Style.navbar_container_right_help}>
+            <p onClick={(e) => openMenu(e)}>Help Center</p>
+            {
+              help && (
+                <div className={Style.navbar_container_right_help_box}>
+                  <HelpCenter />
+                </div>
+              )}
+          </div>
+
+          <div className={Style.navbar_container_right_notify}>
+            <MdNotifications
+            className={Style.notify}
+            onClick={() => openNotification()}
+            />
+
+            {notification && <Notification />}
+          </div>
+
+          <div className={Style.navbar_container_right_button}>
+            <Button btnText="Create" />
+          </div>
+
+          <div className={Style.navbar_container_right_profle_box}>
+            <div className={Style.navbar_container_right_profile}>
+              <Image src={images.user1}
+              alt="Profile"
+              width={40}
+              height={40}
+              onClick={() => openProfile()}
+              className={Style.navbar_container_right_profile}
+              />
+
+              { profile && <Profile /> }
+            </div>
+          </div>
+
+          <div className={Style.navbar_container_right_menuBtn}>
+            <CgMenuRight
+              className={Style.menuIcon}
+              onClick={() => openSideBar()}
+            />
+          </div>
+        </div>
+      </div>
+
+      {
+        openSideMenu && (
+          <div className={Style.Sidebar}>
+            <Sidebar setOpenSideMenu={setOpenSideMenu} />
+          </div>
+        )
+      }
+
+    </div>
   )
 }
 
